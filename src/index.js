f1f0516d2ec66c1b0379e0e3a5c33bff60f426cf
Ernesto Jara <ernesto.jara@nextiva.com>
2018-11-18 20:18:25 -0600

    Adding scripts to deploy on heroku or however you name it

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import cors from "cors";
 
 import rootRoute from "./routes/root";
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 app.use(bodyParse.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use("/api", rootRoute);
 
-app.listen(5000, () => console.log("http://localhost:5000"));
+app.listen(PORT, () => console.log("Up and Running n.n"));
