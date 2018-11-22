23fbd83bc4e887b2a3a313f83cc2b26de3a00746
Ernesto Jara <ernesto.jara@nextiva.com>
2018-11-21 21:50:37 -0600

    Last push, moving to bitbuket hihihi

---
+++ src/config/keys.prod.js
@@ -0,0 +1,4 @@
+export const clientID = process.env.clientID;
+export const clientSecret = process.env.clientSecret;
+export const dbLocalHost = "mongodb://127.0.0.1:27017/" + process.env.DBname;
+export const cookieKey = process.env.secretString;
